0ace9bf0c6cf85a31a6f8f75bc3380024480ba3a
Khairul <wansnap@gmail.com>
2019-05-07 13:22:32 +0800

    New commit

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # arduino_hc06
 
+## Arduino UNO PinOut
 ![Alt text](https://github.com/KhairulIzwan/arduino_hc06/blob/master/img/Introduction-to-Arduino-UNO.png)
+
+## Wiring Diagram
+![Alt text](https://github.com/KhairulIzwan/arduino_hc06/blob/master/img/HC06_LED_Blink.png)
---
+++ led_blink_hc06/led_blink_hc06.ino
@@ -0,0 +1,37 @@
+// Basic use of Bluetooth Serial Transceiver HC06
+
+// Require SoftwareSerial library to convert ordinary digital pins as serial pins (TX, RX)
+#include <SoftwareSerial.h>
+
+// Namespace -- give a specific name for our new serial port
+SoftwareSerial mySerial(3, 4); // RX, TX
+
+// #define constantName value
+#define LED 13;
+
+// Create a variables
+int data = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  mySerial.begin(9600);
+
+  pinMode(LED, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (mySerial.available()){
+    data = mySerial.read();
+  }
+  // Importtant: the '' and "" are different meaning
+  if (data == '1'){
+    digitalWrite(LED, HIGH);
+  }
+  else if (data == '2')
+  {
+    digitalWrite(LED, LOW);
+  }
+
+}
