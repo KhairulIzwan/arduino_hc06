df71e9d41191fe0ac01e5e4c5e497bba89c3c019
Khairul <wansnap@gmail.com>
2019-05-10 00:23:46 +0800

    New commit

--- motor_hc06/motor_hc06.ino
+++ motor_hc06/motor_hc06.ino
@@ -13,7 +13,8 @@ SoftwareSerial mySerial(3, 4); // RX, TX
 #define CCW 8
 #define PWM 6
 
-#define SW 9
+#define SW_OPEN 9
+#define SW_CLOSE 10
 
 // Create a variables
 int data = 0;
@@ -23,12 +24,13 @@ void setup() {
   mySerial.begin(9600);
 
   pinMode(LED, OUTPUT);
-  
+
   pinMode(CW, OUTPUT);
   pinMode(CCW, OUTPUT);
   pinMode(PWM, OUTPUT);
 
-  pinMode(SW, INPUT_PULLUP);
+  pinMode(SW_OPEN, INPUT_PULLUP);
+  pinMode(SW_CLOSE, INPUT_PULLUP);
 
 }
 
@@ -40,11 +42,11 @@ void loop() {
   // Importtant: the '' and "" are different meaning
   if (data == 'A')  // Open
   {
-    if (digitalRead(SW) == LOW)
+    if (digitalRead(SW_OPEN) == LOW)
     {
     // Any action put here!
     digitalWrite(LED, LOW);
-    
+
     digitalWrite(CW, HIGH);
     digitalWrite(CCW, HIGH);
     analogWrite(PWM, 0);
@@ -53,19 +55,19 @@ void loop() {
     {
     // Any action put here!
     digitalWrite(LED, HIGH);
-    
+
     digitalWrite(CW, LOW);
     digitalWrite(CCW, HIGH);
-    analogWrite(PWM, 255);
+    analogWrite(PWM, 50);
     }
   }
   else if (data == 'B') //  Close
   {
-    if (digitalRead(SW) == LOW)
+    if (digitalRead(SW_CLOSE) == LOW)
     {
     // Any action put here!
     digitalWrite(LED, LOW);
-    
+
     digitalWrite(CW, HIGH);
     digitalWrite(CCW, HIGH);
     analogWrite(PWM, 0);
@@ -74,19 +76,19 @@ void loop() {
     {
     // Any action put here!
     digitalWrite(LED, HIGH);
-    
+
     digitalWrite(CW, HIGH);
     digitalWrite(CCW, LOW);
-    analogWrite(PWM, 255);
+    analogWrite(PWM, 50);
     }
-  }
-  else
-  {
-  // Any action put here!
-  digitalWrite(LED, LOW);
-  
-  digitalWrite(CW, HIGH);
-  digitalWrite(CCW, HIGH);
-  analogWrite(PWM, 0);
-  }
+  // }
+  // else
+  // {
+  // // Any action put here!
+  // digitalWrite(LED, LOW);
+  //
+  // digitalWrite(CW, HIGH);
+  // digitalWrite(CCW, HIGH);
+  // analogWrite(PWM, 0);
+  // }
 }
